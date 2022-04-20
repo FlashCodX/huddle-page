d295486b6462f8fc4f1ce19f3d1d23b37d5b8146
FlashCodX <luis.pinto.trading@gmail.com>
2022-04-20 18:50:10 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import styles from "./styles/App.module.css";
+const background = require("./assets/bg-desktop.svg").default;
+const ilustration = require("./assets/illustration-mockups.svg").default;
+const logo = require("./assets/logo.svg").default;
 
-function App() {
+export const App = () => {
+  const {
+    container,
+    imageBackground,
+    content,
+    logoContainer,
+    pageContent,
+    infoContainer,
+    socialsContainer,
+    attribution,
+  } = styles;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <main className={container}>
+        <img className={imageBackground} src={background} alt="background" />
+        <div className={content}>
+          <div className={logoContainer}>
+            <img src={logo} alt="logo" />
+          </div>
 
-export default App;
+          <div className={pageContent}>
+            <img src={ilustration} alt="ilustration" />
+            <div className={infoContainer}>
+              <h1> Build The Community Your Fans Will Love</h1>
+              <p>
+                Huddle re-imagines the way we build communities. You have a
+                voice, but so does your audience. Create connections with your
+                users as you engage in genuine discussion.{" "}
+              </p>
+              <button>Register</button>
+            </div>
+          </div>
+          <div className={socialsContainer}>
+            <div>
+              <FaFacebookF />
+            </div>
+            <div>
+              <FaTwitter />
+            </div>
+            <div>
+              <FaInstagram />
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p className={attribution}>
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/luispintodev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Luís Pinto
+          </a>
+          .
+        </p>
+      </footer>
+    </>
+  );
+};
